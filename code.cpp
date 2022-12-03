a0603c95ae81e27f67a0086b50a11145efe5b442
sanjays661 <90175413+sanjays661@users.noreply.github.com>
2022-12-02 21:29:05 -0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -37,14 +37,12 @@ Node* insert(Node* head, int value){
    {
       cur = cur->next;
    }
-   Node* afterInsertion = cur->next->next;
    Node insertion;
    insertion.data = value;
    insertion.next = cur->next;
-   cur->next = &insertion;
-   insertion.next = *afterInsertion;
    return head;
 }
+
 /* Problem 3: 10 points*/
 
 bool isBalanced(std::string s){
